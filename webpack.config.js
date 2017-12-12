01a3b51ccc323e9a82e2dd4d4d27c0d820fee80f
Talovic <dzevad88@gmail.com>
2017-12-12 12:05:16 +0100

    Setting up importsin app.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/playground/redux-101.js',
+  entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
